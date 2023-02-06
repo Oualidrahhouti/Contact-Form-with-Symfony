8badbea4c862385abd561a719745cb68776098f0
oualid rahhouti <rahhoutiwalid@gmail.com>
2023-02-06 16:54:04 +0100

    adding Internaute and Question Entities

---
+++ src/Entity/Question.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\QuestionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=QuestionRepository::class)
+ */
+class Question
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $question;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $vu;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Internaute::class, inversedBy="questions")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $internaute;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getQuestion(): ?string
+    {
+        return $this->question;
+    }
+
+    public function setQuestion(string $question): self
+    {
+        $this->question = $question;
+
+        return $this;
+    }
+
+    public function isVu(): ?bool
+    {
+        return $this->vu;
+    }
+
+    public function setVu(bool $vu): self
+    {
+        $this->vu = $vu;
+
+        return $this;
+    }
+
+    public function getInternaute(): ?Internaute
+    {
+        return $this->internaute;
+    }
+
+    public function setInternaute(?Internaute $internaute): self
+    {
+        $this->internaute = $internaute;
+
+        return $this;
+    }
+}
